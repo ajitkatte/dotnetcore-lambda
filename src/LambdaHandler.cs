6dbdba750886e961651ef97bb4d4bc6da1c34e12
Ajit Katte <ajit.katte5@gmail.com>
2018-05-27 16:19:27 +0530

    Added dotnetcore2.0 lambda application and unit tests

---
+++ src/LambdaHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Amazon.Lambda.Core;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace LambdaTestApp
+{
+    public class LambdaHandler
+    {
+        public string Handler(string request, ILambdaContext context)
+        {
+            context.Logger.LogLine($"Request : {request}");
+            return $"HELLO {request.ToUpper()}";
+        }
+    }
+}
---
+++ tests/TestApp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xunit;
+using LambdaTestApp;
+using Amazon.Lambda.TestUtilities;
+
+namespace LambdaAppTest
+{
+    public class TestApp
+    {
+        private LambdaHandler _handler;
+        private TestLambdaContext _context;
+
+        public TestApp()
+        {
+            _handler = new LambdaHandler();
+            _context = new TestLambdaContext();
+        }
+
+        [Fact]
+        public void Test_Lambda_Success()
+        {            
+            var response = _handler.Handler("world", _context);
+            Assert.Equal("HELLO WORLD", response);
+        }
+
+        [Fact]
+        public void Test_Lambda_Failure()
+        {
+            var context = new TestLambdaContext();
+            var response = _handler.Handler("Logging", _context);
+            Assert.NotEqual("Hello World", response);
+        }
+    }
+}
